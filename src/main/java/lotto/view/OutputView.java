793d1eee896bec3a96542fa03ead5c114beddec8
PUNGZI <ashyoo1367@gmail.com>
2023-12-02 17:07:25 +0900

    refactor(OutputView): 불필요한 메서드 정리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,26 +1,23 @@
 package lotto.view;
 
+import java.sql.SQLOutput;
 import java.util.List;
 import lotto.common.constants.Message;
 
 public class OutputView {
     private final static String NEW_LINE = System.lineSeparator();
+    private final static String DIVIDING_LINE = "---";
 
     public static void outputTicketCount(int ticketCount) {
         System.out.println(NEW_LINE + ticketCount + Message.OUTPUT_TICKET_COUNT.getMessage());
     }
 
     public static void outputPurchaseTickets(List<List<Integer>> tickets) {
-        tickets.forEach(ticket -> {
-            outputPurchaseTicket(ticket);
-        });
-    }
-
-    private static void outputPurchaseTicket(List<Integer> ticket) {
-        System.out.println(ticket);
+        tickets.forEach(System.out::println);
     }
 
     public static void outputPrizeStat() {
-        System.out.println(Message.OUTPUT_PRIZE_STAT.getMessage());
+        System.out.println(NEW_LINE + Message.OUTPUT_PRIZE_STAT.getMessage());
+        System.out.println(DIVIDING_LINE);
     }
 }
