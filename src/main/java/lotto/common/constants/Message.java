3edfc488deae0deb9bcc36d418fed504ca37c107
PUNGZI <ashyoo1367@gmail.com>
2023-12-07 23:43:33 +0900

    feat(Message): 수익률 출력 메시지 추가

--- src/main/java/lotto/common/constants/Message.java
+++ src/main/java/lotto/common/constants/Message.java
@@ -5,7 +5,8 @@ public enum Message {
     OUTPUT_TICKET_COUNT("개를 구매했습니다."),
     INPUT_LOTTO_NUMBER("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-    OUTPUT_PRIZE_STAT("당첨 통계");
+    OUTPUT_PRIZE_STAT("당첨 통계"),
+    OUTPUT_PROFIT_RATE("총 수익률은 %d%입니다.");
     private final String message;
 
     Message(String message) {
